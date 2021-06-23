e43501b2007ecf230dcf8db4c63a2aae12569ffa
eduardorerick <eduardorerick@gmail.com>
2021-06-23 19:07:54 -0300

    fixed error in line 53 in Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -50,7 +50,7 @@ export function Room() {
     
     roomRef.on('value', room => {
       const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions;
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
       const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
         return {
